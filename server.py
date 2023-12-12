7723ccf3c0d7e4ba54fe2132a67b12043bacbd40
dlouangr <cxe8atj@vjq@MVD*nuc>
2023-12-12 09:26:16 -0500

    Added path for weapons page

--- server.py
+++ server.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/weapons") 
+def weapons(): 
+    return render_template("weapons.html") 
+
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
