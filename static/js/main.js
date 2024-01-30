a930fdcb1f1ecede4260f862bd5c7895b8578782
dlouangr <cxe8atj@vjq@MVD*nuc>
2024-01-30 13:09:57 -0500

    Added simple js for getting all weapons

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,24 @@
+const apiURL = `https://mhw-db.com/weapons`;
+
 /**
  * TO-DO: Method to pull MHW-db and get all weapons on page loading
  */
+async function getWeapons() {
+    try {
+        const response = await fetch(apiURL);
+
+        if (response.ok) {
+            console.log(`Received response: ${response.status}`);
+        } else {
+            console.log(`Error, no response. Status: ${response.status}`);
+        }
+    } catch (error) {
+        console.error(`Error fetching data: ${error}`);
+    }
+}
+
+// Call the function
+getWeapons();
 
 /**
  * TO-DO: Method to poll MHW-db for selected weapon only
