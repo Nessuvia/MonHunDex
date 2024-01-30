9219edf0afed081532bee2f119bb6e217e897827
dlouangr <dlouangr@uncc.edu>
2024-01-30 02:14:44 -0500

    Made skeleton framework for the javascript

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-});
+/**
+ * TO-DO: Method to pull MHW-db and get all weapons on page loading
+ */
+
+/**
+ * TO-DO: Method to poll MHW-db for selected weapon only
+ */
+
+/**
+ * TO-DO: Method to call weapon cards with information
+ */
